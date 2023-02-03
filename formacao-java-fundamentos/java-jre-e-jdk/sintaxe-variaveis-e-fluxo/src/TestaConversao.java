97b6bc1497e0520aea04fb7b69cb141e41f24d60
Renato <rena.f.oliveira@gmail.com>
2023-02-03 20:22:04 -0300

    conversoes e outros tipos

---
+++ formacao-java-fundamentos/java-jre-e-jdk/sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,22 @@
+
+public class TestaConversao {
+
+	public static void main(String[] args) {
+		
+		double salario = 1270.50;
+		
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		long numeroGrande = 32432423523L;
+		short valorPequeno = 2131;
+		byte b = 127;
+		
+		double valor1 = 0.2;
+		double valor2 = 01;
+		
+		System.out.println(valor1 + valor2);
+		
+	}
+	
+}
