34ddb47a560afad3d1731ed9c15a34dcf464e43a
Renato <rena.f.oliveira@gmail.com>
2023-02-04 12:16:16 -0300

    metodos com retorno

--- formacao-java-fundamentos/java-00-parte1/bytebank/src/bytebank/Conta.java
+++ formacao-java-fundamentos/java-00-parte1/bytebank/src/bytebank/Conta.java
@@ -11,4 +11,13 @@ public class Conta {
 		this.saldo += valor;
 	}
 	
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 }
