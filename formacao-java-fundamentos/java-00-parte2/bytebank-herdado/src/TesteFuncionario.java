a2904c376b443866da1becfa2271b3b78e152ddc
Renato <rena.f.oliveira@gmail.com>
2023-02-05 00:15:06 -0300

    modulo heranca e polimorfismo - revisão e criando a classe funcionario

---
+++ formacao-java-fundamentos/java-00-parte2/bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,15 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		
+		Funcionario nico = new Funcionario();
+		nico.setNome("nico steppat");
+		nico.setCpf("223344556-67");
+		nico.setSalario(2600.0);
+		
+		System.out.println(nico.getNome());
+		System.out.println(nico.getBonificacao());
+	}
+
+}
