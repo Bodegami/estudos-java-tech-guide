a42ae518c12de17d3dac9f013e6131bca63d6b72
Renato <rena.f.oliveira@gmail.com>
2023-02-06 14:50:03 -0300

    completando o sistema

--- formacao-java-fundamentos/java-00-parte2/bytebank-herdado/src/Gerente.java
+++ formacao-java-fundamentos/java-00-parte2/bytebank-herdado/src/Gerente.java
@@ -1,10 +1,28 @@
 
-public class Gerente extends Funcionario {
+//Gerente é um Funcionario, Gerente herda da class Funcionario e
+//assina o contrato Autenticavel, Gerente implementa a interface Autenticavel
+public class Gerente extends Funcionario implements Autenticavel {
 
+	private int senha;
+	
 	@Override
 	public double getBonificacao() {
 		System.out.println("chamando o metodo bonificacao do GERENTE");
 		return super.getSalario();
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		if (this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
