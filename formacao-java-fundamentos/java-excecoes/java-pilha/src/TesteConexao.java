f3f9d48de37fe2757a683393c96555cbd8cf3a07
Renato <rena.f.oliveira@gmail.com>
2023-02-07 20:56:42 -0300

    try with resources && fim do modulo

--- formacao-java-fundamentos/java-excecoes/java-pilha/src/TesteConexao.java
+++ formacao-java-fundamentos/java-excecoes/java-pilha/src/TesteConexao.java
@@ -3,15 +3,29 @@ public class TesteConexao {
 
 	public static void main(String[] args) {
 		
-		Conexao con = null;
-		try {
-			con = new Conexao();
-			con.leDados();
-		} catch (IllegalStateException e) {
+		
+		// forma mais atualizada
+		try (Conexao conexao = new Conexao()) {
+			conexao.leDados();
+		} catch(Exception ex) {
 			System.out.println("Deu erro na conexao");
-		} finally {
-			con.fecha();
 		}
+		
+
+		// ------------------------------------------------
+		// Um try, sempre exige UM catch ou UM finally
+//		Conexao con = null;
+//		try {
+//			con = new Conexao();
+//			con.leDados();
+//		} catch (IllegalStateException e) {
+//			System.out.println("Deu erro na conexao");
+//		} finally {
+//			System.out.println("finally..");
+//			if (con != null) {
+//				con.close();
+//			}
+//		}
 		
 	}
 	
