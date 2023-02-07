27a98a240df02b5c0d3a288c4afeb6d2c09ece93
Renato <rena.f.oliveira@gmail.com>
2023-02-07 20:40:23 -0300

    sacando com Checked Exception

--- formacao-java-fundamentos/java-00-parte2/bytebank-herdado-conta/src/TesteSaca.java
+++ formacao-java-fundamentos/java-00-parte2/bytebank-herdado-conta/src/TesteSaca.java
@@ -6,11 +6,14 @@ public class TesteSaca {
 		ContaCorrente conta = new ContaCorrente(123, 321);
 		
 		conta.deposita(200.0);
-		conta.saca(210.0);
+		
+		try {
+			conta.saca(210.0);
+		} catch(Exception ex) {
+			System.out.println("Ex: " +ex.getMessage());
+		}
 		
 		System.out.println(conta.getSaldo());
-		
-
 	}
 
 }
