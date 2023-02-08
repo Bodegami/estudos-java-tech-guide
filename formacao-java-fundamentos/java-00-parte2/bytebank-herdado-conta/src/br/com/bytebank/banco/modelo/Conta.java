9abaf04ead77bfb4516b8efcac08e34e621c1188
Renato <rena.f.oliveira@gmail.com>
2023-02-08 16:27:42 -0300

    conhecendo o javadoc e gerando a documentacao do ByteBank

--- formacao-java-fundamentos/java-00-parte2/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ formacao-java-fundamentos/java-00-parte2/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -1,5 +1,12 @@
 package br.com.bytebank.banco.modelo;
 
+/**
+ * Classe que representa a moldura de uma conta.
+ * 
+ * @author renaf
+ * @version 0.1
+ *
+ */
 public abstract class Conta {
 	
 	protected double saldo;
@@ -8,6 +15,12 @@ public abstract class Conta {
 	private Cliente titular;
 	private static int total;
 
+	/**
+	 * Construtor para inicializar o objeto Conta a partir da agencia e numero
+	 * 
+	 * @param agencia
+	 * @param numero
+	 */
 	public Conta(int agencia, int numero) {
 		total++;
 		//System.out.println("o total de contas é " + total);
@@ -19,6 +32,12 @@ public abstract class Conta {
 	
 	public abstract void deposita(double valor);
 	
+	/**
+	 * Valor precisa ser maior que o saldo.
+	 * 
+	 * @param valor
+	 * @throws SaldoInsuficienteException
+	 */
 	public void saca(double valor) throws SaldoInsuficienteException {
 		
 		if (this.saldo < valor) {
