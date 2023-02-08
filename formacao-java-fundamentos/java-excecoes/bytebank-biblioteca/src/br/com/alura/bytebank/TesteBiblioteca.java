87961c9a4fd2494148c577cd47a06d05aea21cb3
Renato <rena.f.oliveira@gmail.com>
2023-02-08 16:51:53 -0300

    criando um JAR executavel

--- formacao-java-fundamentos/java-excecoes/bytebank-biblioteca/src/br/com/alura/bytebank/TesteBiblioteca.java
+++ formacao-java-fundamentos/java-excecoes/bytebank-biblioteca/src/br/com/alura/bytebank/TesteBiblioteca.java
@@ -6,6 +6,23 @@ import br.com.bytebank.banco.modelo.ContaCorrente;
 public class TesteBiblioteca {
 	
 	/*
+	 * GERANDO UM .JAR:
+	 * 1 - clicamos com o botao direito na raiz do projeto e selecionamos o 'export'
+	 * 2 - depois selecionar java -> JAR file
+	 * 3 - selecionar o que deseja importar. ex: src, docs, .classpath e etc
+	 * 4 - avançamos e devemos apontar o nome e o destino do .jar
+	 * 5 - se a intenção for criar apenas uma lib, podemos clicar em finish e estará disponivel para usar.
+	 * 6 - se a intenção for criar um .jar executavel, clicamos em 'next' e depois 'next' novamente.
+	 * 7 - agora precisamos definir qual entry-point desse .jar, ou seja, qual arquivo main que ele deve executar.
+	 * 8 - pronto!
+	 * 
+	 * 
+	 * EXECUTANDO UM .JAR:
+	 * 1 - atraves do terminal, vamos até o diretorio onde está o .jar.
+	 * 2 - agora executamos o comando: java -jar <nome_do_jar>.jar
+	 * 3 - pronto! A saida do será impressa no terminal.
+	 * 
+	 * 
 	 * IMPORTANDO UMA LIB:
 	 * 1 - criar um diretorio chamado 'lib'
 	 * 2 - copiar o .jar para dentro do diretorio 'lib'
