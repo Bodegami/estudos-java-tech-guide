bd7cffd5fbf17693552bea06ef02565942249e2b
Renato <rena.f.oliveira@gmail.com>
2023-02-08 16:57:01 -0300

    conhecendo o java.lang

--- formacao-java-fundamentos/java-00-parte2/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteContas.java
+++ formacao-java-fundamentos/java-00-parte2/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteContas.java
@@ -3,13 +3,15 @@ package br.com.bytebank.banco.test;
 import br.com.bytebank.banco.especial.ContaEspecial;
 import br.com.bytebank.banco.modelo.*;
 
+//br.com.bytebank.banco.test.TesteContas
 public class TesteContas {
 
+	//java.lang.String
 	public static void main(String[] args) throws SaldoInsuficienteException {
 		
 		ContaEspecial ce = new ContaEspecial(123, 5555);
 		
-		//Full Quualified Name (FQN) == br.com.bytebank.banco.modelo.ContaCorrente
+		//Full Qualified Name (FQN) == br.com.bytebank.banco.modelo.ContaCorrente
 		ContaCorrente cc = new ContaCorrente(111, 111);
 		cc.deposita(100.0);
 		
