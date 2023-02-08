e8aadaa70cafe9123772abce752eb14f56026ede
Renato <rena.f.oliveira@gmail.com>
2023-02-08 11:25:19 -0300

    gerenciando pacotes com eclipse

--- formacao-java-fundamentos/java-00-parte2/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/SaldoInsuficienteException.java
+++ formacao-java-fundamentos/java-00-parte2/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/SaldoInsuficienteException.java
@@ -1,6 +1,7 @@
 package br.com.bytebank.banco.modelo;
 
 public class SaldoInsuficienteException extends Exception {
+	private static final long serialVersionUID = 1555899283889752904L;
 
 	public SaldoInsuficienteException(String msg) {
 		super(msg);
