1099b7f2250ba22aeb5b473f1f32c97d3badf94c
Renato <rena.f.oliveira@gmail.com>
2023-02-09 15:48:01 -0300

    array do tipo Object

--- formacao-java-fundamentos/java-00-parte2/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
+++ formacao-java-fundamentos/java-00-parte2/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
@@ -1,31 +1,46 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Cliente;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 
-		ContaCorrente[] contas = new ContaCorrente[5];
+		//Conta[] contas = new Conta [5];
+		Object[] referencias = new Object [5];
 		
 		//o valor padrao de referencias é null
-		for (int i = 0; i < contas.length; i++) {
-			System.out.println(contas[i]);
+		for (int i = 0; i < referencias.length; i++) {
+			System.out.println(referencias[i]);
 		}
 		
 		ContaCorrente cc1 = new ContaCorrente(22, 11);
-		contas[0] = cc1;
-		System.out.println(contas[0]);
+		referencias[0] = cc1;
+		System.out.println(referencias[0]);
 		
-		ContaCorrente cc2 = new ContaCorrente(22, 11);
-		contas[1] = cc2;
+		ContaPoupanca cc2 = new ContaPoupanca(22, 11);
+		referencias[1] = cc2;
 		
 		//System.out.println(cc2.getNumero());
-		System.out.println(contas[1].getNumero());
 		
-		ContaCorrente ref = contas[1];
+		//Object referenciaGenerica = contas[1];
+		// Não funciona, pois o compilador sabe que não existe o metodo getNumero() na classe Object
+		//System.out.println(referenciaGenerica.getNumero()); 
+		
+		// Conta ref = contas[1]; -> o compilador ñ consegue verificar o tipo correto, por isso ele aponta 
+		//para o tipo mais generico
+		ContaPoupanca ref = (ContaPoupanca) referencias[1]; //type cast
 		System.out.println(cc2.getNumero());
 		System.out.println(ref.getNumero());
+		
+		//ContaCorrente ref = (ContaCorrente) contas[1]; -> retorna a excecao ClassCastException
+		System.out.println(ref);
+		
+		Cliente cliente = new Cliente();
+		referencias[2] = cliente;
+		System.out.println(referencias[2]);
 		
 	}
 
