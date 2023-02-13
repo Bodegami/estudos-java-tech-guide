89ceac4daed0391316dd4f9abc0e13ad5bee6aa2
Renato <rena.f.oliveira@gmail.com>
2023-02-13 19:18:06 -0300

    aplicando o Set no modelo

--- formacao-java-fundamentos/java-collections/gerenciador-de-cursos/src/br/com/bode/Curso.java
+++ formacao-java-fundamentos/java-collections/gerenciador-de-cursos/src/br/com/bode/Curso.java
@@ -2,18 +2,19 @@ package br.com.bode;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new ArrayList<>();
+	private Set<Aluno> alunos = new HashSet<>();
 	private int tempoTotalCurso;
 
-	public int getTempoTotalCurso() {
-		return tempoTotalCurso;
-	}
+
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -38,6 +39,10 @@ public class Curso {
 		this.aulas.add(aula);
 		this.tempoTotalCurso += aula.getTempo();
 	}
+	
+	public int getTempoTotalCurso() {
+		return tempoTotalCurso;
+	}
 
 	public int getTempoTotal() {
 //		int tempoTotal = 0;
@@ -46,13 +51,22 @@ public class Curso {
 //		}
 //		return tempoTotal;
 		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
-
 	}
+	
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
+	}
+
 	
 	@Override
 	public String toString() {
 		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() 
 		+ ", aulas: " + this.aulas + "]";
+	}
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+		
 	}
 
 }
