43c8872875d094e2c7da055c99abc9d327d7c5b9
Renato <rena.f.oliveira@gmail.com>
2023-02-16 00:12:08 -0300

    testando um metodo que lanca exception

--- formacao-java-fundamentos/java-tdd/java-testes-unitarios-tdd/src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ formacao-java-fundamentos/java-tdd/java-testes-unitarios-tdd/src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -7,7 +7,10 @@ import br.com.alura.tdd.modelo.Funcionario;
 
 public class ReajusteService {
 	
-	//Pattern Strategy
+	//Pattern utilizado Strategy
+	
+	//O TDD é interessante de se aplicar quando temos cenarios que ainda não estão muito
+	//claros em como vai ficar o codigo, quantas classes vou precisar e etc
 
 	public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
 		BigDecimal percentual = desempenho.percentualReajuste();
