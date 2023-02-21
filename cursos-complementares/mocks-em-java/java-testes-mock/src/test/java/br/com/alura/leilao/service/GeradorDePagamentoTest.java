8a8065d33702d1a54286925c405116b18c450469
Renato <rena.f.oliveira@gmail.com>
2023-02-21 11:40:28 -0300

    capturando objetos com o Mockito

--- cursos-complementares/mocks-em-java/java-testes-mock/src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
+++ cursos-complementares/mocks-em-java/java-testes-mock/src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -1,9 +1,13 @@
 package br.com.alura.leilao.service;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -11,6 +15,7 @@ import org.mockito.MockitoAnnotations;
 import br.com.alura.leilao.dao.PagamentoDao;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Pagamento;
 import br.com.alura.leilao.model.Usuario;
 
 class GeradorDePagamentoTest {
@@ -19,6 +24,9 @@ class GeradorDePagamentoTest {
 	
 	@Mock
 	private PagamentoDao pagamentoDao;
+	
+	@Captor
+	private ArgumentCaptor<Pagamento> captor;
 	
 	@BeforeEach
 	public void setup() {
@@ -32,7 +40,17 @@ class GeradorDePagamentoTest {
 		Lance vencedor = leilao.getLanceVencedor();
 		gerador.gerarPagamento(vencedor);
 		
-		Mockito.verify(pagamentoDao).salvar(Mockito.any());
+		//Utilizamos essa abordagem quando queremos capturar um objeto que é criado dentro de um metodo
+		Mockito.verify(pagamentoDao).salvar(captor.capture());
+		
+		//Com o captor conseguimos capturar o objeto no momento em que ele é passado o mock
+		Pagamento pagamento = captor.getValue();
+		
+		Assertions.assertEquals(LocalDate.now().plusDays(1), pagamento.getVencimento());
+		Assertions.assertEquals(vencedor.getValor(), pagamento.getValor());
+		Assertions.assertFalse(pagamento.getPago());
+		Assertions.assertEquals(vencedor.getUsuario(), pagamento.getUsuario());
+		Assertions.assertEquals(leilao, pagamento.getLeilao());
 	}
 	
 	private Leilao leilao() {
