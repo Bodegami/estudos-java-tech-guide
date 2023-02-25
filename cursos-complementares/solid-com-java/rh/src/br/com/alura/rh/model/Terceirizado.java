d8068b22cb9871690e61f57854f1c0eec6f4c718
Renato <rena.f.oliveira@gmail.com>
2023-02-25 19:17:00 -0300

    sobre Liskov Substitution Principle

--- cursos-complementares/solid-com-java/rh/src/br/com/alura/rh/model/Terceirizado.java
+++ cursos-complementares/solid-com-java/rh/src/br/com/alura/rh/model/Terceirizado.java
@@ -2,12 +2,14 @@ package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
 
-public class Terceirizado extends Funcionario {
+public class Terceirizado {
 	
+	private DadosPessoais dadosPessoais;
 	private String empresa;
 
-	public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-		super(nome, cpf, cargo, salario);
+	public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario, String empresa) {
+		this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
+		this.empresa = empresa;
 	}
 
 	public String getEmpresa() {
@@ -17,7 +19,37 @@ public class Terceirizado extends Funcionario {
 	public void setEmpresa(String empresa) {
 		this.empresa = empresa;
 	}
-	
-	
+
+	public String getNome() {
+		return dadosPessoais.getNome();
+	}
+
+	public void setNome(String nome) {
+		this.dadosPessoais.setNome(nome);
+	}
+
+	public String getCpf() {
+		return dadosPessoais.getCpf();
+	}
+
+	public void setCpf(String cpf) {
+		this.setCpf(cpf);
+	}
+
+	public Cargo getCargo() {
+		return dadosPessoais.getCargo();
+	}
+
+	public void setCargo(Cargo cargo) {
+		this.setCargo(cargo);
+	}
+
+	public BigDecimal getSalario() {
+		return dadosPessoais.getSalario();
+	}
+
+	public void setSalario(BigDecimal salario) {
+		this.setSalario(salario);
+	}
 
 }
