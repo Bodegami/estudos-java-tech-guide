1550757fe7ffae0b2e4d229ccd974b26df6f51fc
Renato <rena.f.oliveira@gmail.com>
2023-02-26 00:08:26 -0300

    calculando impostos

---
+++ cursos-complementares/design-pattern-em-java-1/loja/src/br/com/alura/loja/TestesImpostos.java
@@ -0,0 +1,21 @@
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.TipoImposto;
+
+public class TestesImpostos {
+
+	public static void main(String[] args) {
+		
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+		
+		System.out.println(calculadora.calcular(orcamento, TipoImposto.ICMS));
+		System.out.println(calculadora.calcular(orcamento, TipoImposto.ISS));
+		
+
+	}
+
+}
