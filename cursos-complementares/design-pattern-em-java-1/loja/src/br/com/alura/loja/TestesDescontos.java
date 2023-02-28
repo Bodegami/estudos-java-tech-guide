851212bb31c5764c4e402c3760e4159fc1b1aae6
Renato <rena.f.oliveira@gmail.com>
2023-02-27 23:23:49 -0300

    calculando descontos

---
+++ cursos-complementares/design-pattern-em-java-1/loja/src/br/com/alura/loja/TestesDescontos.java
@@ -0,0 +1,19 @@
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.desconto.CalculadoraDeDescontos;
+
+public class TestesDescontos {
+
+	public static void main(String[] args) {
+
+		Orcamento orcamentoPrimeiro = new Orcamento(new BigDecimal("200"), 6);
+		Orcamento orcamentoSegundo = new Orcamento(new BigDecimal("1000"), 1);
+		CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
+
+		System.out.println(calculadora.calcular(orcamentoPrimeiro));
+		System.out.println(calculadora.calcular(orcamentoSegundo));
+	}
+
+}
