d975b8caa2d2047dc292b9504afa5ef73f1b001d
Renato <rena.f.oliveira@gmail.com>
2023-03-01 23:09:17 -0300

    entendendo o pattern State

--- cursos-complementares/design-pattern-em-java-1/loja/src/br/com/alura/loja/orcamento/situacao/SituacaoOrcamento.java
+++ cursos-complementares/design-pattern-em-java-1/loja/src/br/com/alura/loja/orcamento/situacao/SituacaoOrcamento.java
@@ -7,6 +7,20 @@ import br.com.alura.loja.orcamento.Orcamento;
 
 public abstract class SituacaoOrcamento {
 	
+	/**
+	 * Pattern State:
+	 * 
+	 * É bem parecido com o Strategy, porém ele representa o justamente a questão de estado.
+	 * Esse padrão é utilizado quando temos um transição de estados ou quando a gente precisa
+	 * aplicar um regra ou algoritmo, baseado num estado de um objeto ou uma classe.
+	 * 
+	 * Com isso eliminamos if's e else's e conseguimos aplicar regras mais bem definidas para
+	 * cada tipo de transicao de estado.
+	 * 
+	 * 
+	 */
+	
+	
 	public BigDecimal calcularValorDescontoExtra(Orcamento orcamento) {
 		return BigDecimal.ZERO;
 	}
