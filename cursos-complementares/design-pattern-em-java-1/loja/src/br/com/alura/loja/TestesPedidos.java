a52061b13d78a8ffe2cb467797875299842b008f
Renato <rena.f.oliveira@gmail.com>
2023-03-04 17:48:05 -0300

    sobre o Command Handler

--- cursos-complementares/design-pattern-em-java-1/loja/src/br/com/alura/loja/TestesPedidos.java
+++ cursos-complementares/design-pattern-em-java-1/loja/src/br/com/alura/loja/TestesPedidos.java
@@ -3,8 +3,34 @@ package br.com.alura.loja;
 import java.math.BigDecimal;
 
 import br.com.alura.loja.pedido.GeraPedido;
+import br.com.alura.loja.pedido.GeraPedidoHandler;
 
 public class TestesPedidos {
+	
+	/**
+	 * Pattern COMMAND:
+	 * 
+	 * É um pattern que representa a execucao de um comando que precisa ser executado.
+	 * Na verdade utilizamos esse padrao quanto temos varias classes que contem um método
+	 * em comum, como o metodo executa da classe GeraPedido e essa classe implementaria
+	 * uma interface com a mesma assinatura. Então todas as classes com a mesma assinatura
+	 * de metodo implementariam essa interface.
+	 * Note que no padrão COMMAND, a classe além de ter o método executa, ela tem também
+	 * os dados e as dependencias necessárias para execução.
+	 * 
+	 * 
+	 * 
+	 * Pattern COMMAND HANDLER:
+	 * 
+	 * É uma variação do Pattern COMMAND, porém no COMMAND HANDLER os dados são separados
+	 * da classe que implementa o metodo executa.
+	 * Aqui no projeto a classe GeraPedidos é responsavel pelos dados, enquanto a classe 
+	 * GeraPedidoHandler implementa o metodo executa.
+	 * Veja que criamos um objeto do tipo GeraPedido contendo os dados de entrada e depois
+	 * criamos um objeto do tipo GeraPedidoHandler, chamando o metodo executa passando o
+	 * objeto do tipo GeraPedido como parametro.
+	 * 
+	 */
 	
 	public static void main(String[] args) {
 		
@@ -13,7 +39,8 @@ public class TestesPedidos {
 		int quantidadeItens = Integer.parseInt(args[2]);
 		
 		GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
-		gerador.executa();
+		GeraPedidoHandler handler = new GeraPedidoHandler(/*dependencias*/);
+		handler.executa(gerador);
 	}
 
 }
--- cursos-complementares/design-pattern-em-java-1/loja/src/br/com/alura/loja/pedido/GeraPedido.java
+++ cursos-complementares/design-pattern-em-java-1/loja/src/br/com/alura/loja/pedido/GeraPedido.java
@@ -1,9 +1,6 @@
 package br.com.alura.loja.pedido;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-import br.com.alura.loja.orcamento.Orcamento;
 
 public class GeraPedido {
 
@@ -11,19 +8,32 @@ public class GeraPedido {
 	private BigDecimal valorOrcamento;
 	private int quantidadeItens;
 
+	// Injecao de dependencias: PedidoRepository, EmailService e etc...
 	public GeraPedido(String cliente, BigDecimal valorOrcamento, int quantidadeItens) {
 		this.cliente = cliente;
 		this.valorOrcamento = valorOrcamento;
 		this.quantidadeItens = quantidadeItens;
 	}
 
-	public void executa() {
-		Orcamento orcamento = new Orcamento(this.valorOrcamento, this.quantidadeItens);
-		
-		Pedido pedido = new Pedido(cliente, LocalDateTime.now(), orcamento);
+//	public void executa() {
+//		Orcamento orcamento = new Orcamento(this.valorOrcamento, this.quantidadeItens);
+//		
+//		Pedido pedido = new Pedido(cliente, LocalDateTime.now(), orcamento);
+//
+//		System.out.println("Salvar pedido no Banco de Dados");
+//		System.out.println("Enviar email com dados do novo pedido");
+//	}
 
-		System.out.println("Salvar pedido no Banco de Dados");
-		System.out.println("Enviar email com dados do novo pedido");
+	public String getCliente() {
+		return cliente;
+	}
+
+	public BigDecimal getValorOrcamento() {
+		return valorOrcamento;
+	}
+
+	public int getQuantidadeItens() {
+		return quantidadeItens;
 	}
 
 }
