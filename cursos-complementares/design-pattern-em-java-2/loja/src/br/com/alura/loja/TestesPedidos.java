c75153b1236eb65690fda929991aa048b70d0298
Renato <rena.f.oliveira@gmail.com>
2023-03-07 23:07:43 -0300

    aplicando o pattern Facade

--- cursos-complementares/design-pattern-em-java-2/loja/src/br/com/alura/loja/TestesPedidos.java
+++ cursos-complementares/design-pattern-em-java-2/loja/src/br/com/alura/loja/TestesPedidos.java
@@ -12,7 +12,21 @@ import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 public class TestesPedidos {
 	
 	/**
-	 * Pattern :
+	 * Pattern FACADE:
+	 * 
+	 * A ideia dele é ter uma fachada. Basicamente, quando temos
+	 * um processo complexo, que envolve varias etapas, varias
+	 * classes, varias chamadas de metodos, ao inves de expormos
+	 * essa complexidade para o cliente e ele ter que saber quais 
+	 * objetos tem que chamar, a ordem e tudos mais, ele simplesmente
+	 * chama uma fachada, chama um metodo que faz o encapsulamento de 
+	 * toda essa complexidade.
+	 * Assim livramos o cliente de ter que saber toda a complexidade
+	 * da execucao, além de ficamos livres para alterar a implementacao
+	 * com menos risco de prejudicar um cliente que utiliza essa classe.
+	 * 
+	 * Simplesmente, abstraimos e encapsulamos toda complexidade e 
+	 * disponibilizamos uma interface, um metodo que para o cliente.
 	 * 
 	 * 
 	 */
