de0f0ece160c367cb540c97ccd47ce990efdb617
Renato <rena.f.oliveira@gmail.com>
2023-03-07 23:24:37 -0300

    aplicando o pattern Proxy

--- cursos-complementares/design-pattern-em-java-2/loja/src/br/com/alura/loja/TestesComposicao.java
+++ cursos-complementares/design-pattern-em-java-2/loja/src/br/com/alura/loja/TestesComposicao.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.orcamento.OrcamentoProxy;
 
 public class TestesComposicao {
 	
@@ -23,6 +24,24 @@ public class TestesComposicao {
 	 * o comportamento no final. No composite temos um objeto 
 	 * compondo o outro, ou seja, temos uma herarquia de objetos.
 	 * 
+	 * 
+	 * Pattern PROXY:
+	 * 
+	 * É a ideia do proxy de redes. Nos temos um objeto e criamos 
+	 * um proxy que vai criar uma camada que vai ficar interceptando
+	 * as chamadas ao meu objeto original e com isso conseguimos
+	 * fazer validações, otimizações, verificações e enfim, não so
+	 * para performance, mas em qualquer situação que precisamos 
+	 * interceptar um objeto para fazer algum tratamento e você
+	 * quer talvez armazenar isso numa informacao, então podemos
+	 * usar um objeto proxy.
+	 * 
+	 * A propria JPA trabalha com esse conceito. Quando fazemos um select
+	 * no banco de alguma classe que tem relacionamento, a JPA devolve
+	 * somente o objeto da chamada, para devolver os dados do relacionamento
+	 * seria necessario fazer um get nesse dado, ou seja, o hibernate devolve 
+	 * um proxy desse relacionamento.
+	 * 
 	 * 	 
 	 */
 
@@ -36,8 +55,13 @@ public class TestesComposicao {
 		novo.adicionarItem(new ItemOrcamento(new BigDecimal("500")));
 		novo.adicionarItem(antigo);
 		
-		System.out.println(novo.getValor());
-		System.out.println(novo.getValor());
+		OrcamentoProxy proxy = new OrcamentoProxy(novo);
+		
+		System.out.println(proxy.getValor());
+		System.out.println(proxy.getValor());
+		System.out.println(proxy.getValor());
+		System.out.println(proxy.getValor());
+		System.out.println(proxy.getValor());
 		
 	}
 	
