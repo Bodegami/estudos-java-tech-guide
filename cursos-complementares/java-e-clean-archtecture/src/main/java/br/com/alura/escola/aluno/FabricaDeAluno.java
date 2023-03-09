152e6871af5a4f76de75ca22d38f5a9ce91b646c
Renato <rena.f.oliveira@gmail.com>
2023-03-08 23:43:09 -0300

    organizacao das classes

--- cursos-complementares/java-e-clean-archtecture/src/main/java/br/com/alura/escola/aluno/FabricaDeAluno.java
+++ cursos-complementares/java-e-clean-archtecture/src/main/java/br/com/alura/escola/aluno/FabricaDeAluno.java
@@ -1,4 +1,4 @@
-package escola;
+package br.com.alura.escola.aluno;
 
 //Pattern BUILDER
 public class FabricaDeAluno {
--- cursos-complementares/java-e-clean-archtecture/src/main/java/br/com/alura/escola/indicacao/Indicacao.java
+++ cursos-complementares/java-e-clean-archtecture/src/main/java/br/com/alura/escola/indicacao/Indicacao.java
@@ -1,4 +1,4 @@
-package escola;
+package br.com.alura.escola.indicacao;
 
 import java.time.LocalDateTime;
 
