b02032775dad76a9b5760496c3a39f02ae61a188
Renato <rena.f.oliveira@gmail.com>
2023-03-13 22:44:40 -0300

    metodos GET e POST

--- formacao-spring-framework/java-servlet-programacao-web-java/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ formacao-spring-framework/java-servlet-programacao-web-java/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -13,9 +13,12 @@ import javax.servlet.http.HttpServletResponse;
 public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
+		
 		String nomeEmpresa = request.getParameter("nome");
+		
 		System.out.format("Cadastrando nova empresa %s", nomeEmpresa);
 		out.println(String.format("<html><body>Empresa %s cadastrada com sucesso!</body></html>", nomeEmpresa));
 	}
