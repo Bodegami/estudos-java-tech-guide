26f86ac675b71eb2ee0a4a9b6589ebe306eaf5d9
Renato <rena.f.oliveira@gmail.com>
2023-03-14 22:58:30 -0300

    despachando a requisicao

--- formacao-spring-framework/java-servlet-programacao-web-java/WebContent/novaEmpresaCriada.jsp
+++ formacao-spring-framework/java-servlet-programacao-web-java/WebContent/novaEmpresaCriada.jsp
@@ -1,10 +1,12 @@
 <%
   // Java Server Pages = JSP
+  //Todo o código (scriptlet) é executado no servidor antes de enviar a resposta para o navegador.
+
   // A variavel 'out' do PrintWriter continua disponivel aqui
   // out.println(nomeEmpresa);
   // '<%= nomeEmpresa' tem a mesma funcao que 'out.println(nomeEmpresa)'
 
-  String nomeEmpresa = "Alura";
+  String nomeEmpresa = (String) request.getAttribute("empresa");
   System.out.println(nomeEmpresa);
 %>
 
--- formacao-spring-framework/java-servlet-programacao-web-java/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ formacao-spring-framework/java-servlet-programacao-web-java/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,8 +1,8 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,7 +15,7 @@ public class NovaEmpresaServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
+		//PrintWriter out = response.getWriter();
 		
 		String nomeEmpresa = request.getParameter("nome");
 		
@@ -25,7 +25,13 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		System.out.format("Cadastrando nova empresa %s", nomeEmpresa);
+		
+		//Chamar o JSP
+		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		
+		//Atribuimos o valor a request antes de enviar a request para o JSP
+		request.setAttribute("empresa", empresa.getNome());
+		rd.forward(request, response);
 		
 	}
 
