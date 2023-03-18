7475be274996cfa195d6c58d1a19d2a4f7a73427
Renato <rena.f.oliveira@gmail.com>
2023-03-18 18:26:53 -0300

    escondendo o JSP

--- formacao-spring-framework/java-servlet-autenticacao-autorizacao-e-o-padrao-mvc/gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ formacao-spring-framework/java-servlet-autenticacao-autorizacao-e-o-padrao-mvc/gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -13,6 +13,7 @@ import br.com.alura.gerenciador.acao.AlteraEmpresa;
 import br.com.alura.gerenciador.acao.ListaEmpresas;
 import br.com.alura.gerenciador.acao.MostraEmpresa;
 import br.com.alura.gerenciador.acao.NovaEmpresa;
+import br.com.alura.gerenciador.acao.NovaEmpresaForm;
 import br.com.alura.gerenciador.acao.RemoveEmpresa;
 
 @WebServlet("/entrada")
@@ -26,40 +27,34 @@ public class UnicaEntradaServlet extends HttpServlet {
 		String nome = null;
 		
 		if (paramAcao.equals("ListaEmpresas")) {
-
 			ListaEmpresas acao = new ListaEmpresas();
 			nome = acao.executa(request, response);
-			
 		}
 		else if (paramAcao.equals("RemoveEmpresa")) {
-			
 			RemoveEmpresa acao = new RemoveEmpresa();
 			nome = acao.executa(request, response);
-			
 		}
 		else if (paramAcao.equals("MostraEmpresa")) {
-			
 			MostraEmpresa acao = new MostraEmpresa();
 			nome = acao.executa(request, response);
-			
 		}
 		else if (paramAcao.equals("AlteraEmpresa")) {
-			
 			AlteraEmpresa acao = new AlteraEmpresa();
 			nome = acao.executa(request, response);
-			
 		}
 		else if (paramAcao.equals("NovaEmpresa")) {
-			
 			NovaEmpresa acao = new NovaEmpresa();
 			nome = acao.executa(request, response);
-			
+		}
+		else if (paramAcao.equals("NovaEmpresaForm")) {
+			NovaEmpresaForm acao = new NovaEmpresaForm();
+			nome = acao.executa(request, response);
 		}
 		
 		String[] tipoEEndereco = nome.split(":");
 		
-		if (tipoEEndereco[0].equals("foward")) {
-			RequestDispatcher rd = request.getRequestDispatcher(tipoEEndereco[1]);
+		if (tipoEEndereco[0].equals("forward")) {
+			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/view/" + tipoEEndereco[1]);
 			rd.forward(request, response);
 		}
 		else {
