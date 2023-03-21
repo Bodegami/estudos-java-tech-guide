c18c8065a7f064295ef1f8665dd67e41adff516f
Renato <rena.f.oliveira@gmail.com>
2023-03-21 19:43:00 -0300

    ordem de execucao dos filtros pelo web.xml

--- formacao-spring-framework/java-servlet-autenticacao-autorizacao-e-o-padrao-mvc/gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ formacao-spring-framework/java-servlet-autenticacao-autorizacao-e-o-padrao-mvc/gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.alura.gerenciador.acao.Acao;
 
-@WebServlet("/entrada")
+//@WebServlet("/entrada")
 public class UnicaEntradaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -42,6 +41,34 @@ public class UnicaEntradaServlet extends HttpServlet {
 			response.sendRedirect(tipoEEndereco[1]);
 		}
 		
+
+		
+//		if (paramAcao.equals("ListaEmpresas")) {
+//			ListaEmpresas acao = new ListaEmpresas();
+//			nome = acao.executa(request, response);
+//		}
+//		else if (paramAcao.equals("RemoveEmpresa")) {
+//			RemoveEmpresa acao = new RemoveEmpresa();
+//			nome = acao.executa(request, response);
+//		}
+//		else if (paramAcao.equals("MostraEmpresa")) {
+//			MostraEmpresa acao = new MostraEmpresa();
+//			nome = acao.executa(request, response);
+//		}
+//		else if (paramAcao.equals("AlteraEmpresa")) {
+//			AlteraEmpresa acao = new AlteraEmpresa();
+//			nome = acao.executa(request, response);
+//		}
+//		else if (paramAcao.equals("NovaEmpresa")) {
+//			NovaEmpresa acao = new NovaEmpresa();
+//			nome = acao.executa(request, response);
+//		}
+//		else if (paramAcao.equals("NovaEmpresaForm")) {
+//			NovaEmpresaForm acao = new NovaEmpresaForm();
+//			nome = acao.executa(request, response);
+//		}
+		
+
 	}
 
 }
