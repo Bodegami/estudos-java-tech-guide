62d373f25e8000d7e05a99179883c791b2441f2f
Renato <rena.f.oliveira@gmail.com>
2023-03-22 18:16:13 -0300

    criando objetos corretamente

---
+++ formacao-spring-framework/java-reflection-1-entendendo-a-metaprogramacao/alurator-playground/src/br/com/alura/alurator/playground/reflexao/TesteInstanciaObjetoCorretamente.java
@@ -0,0 +1,41 @@
+package br.com.alura.alurator.playground.reflexao;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import br.com.alura.alurator.playground.controle.SubControle;
+
+public class TesteInstanciaObjetoCorretamente {
+
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, 
+				NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
+		
+		Class<SubControle> subControleClasse1 =  SubControle.class; 
+		
+		Class<?> subControleClasse2 = 
+				Class.forName("br.com.alura.alurator.playground.controle.SubControle");
+
+		Class<?> controleClasse1 = 
+				Class.forName("br.com.alura.alurator.playground.controle.Controle");
+
+		//getConstructor() retorna qualquer construtor publico
+		Constructor<SubControle> constructorSubControle = subControleClasse1.getDeclaredConstructor();
+		System.out.println(constructorSubControle);
+		
+		//getDeclaredConstructor() retorna qualquer construtor de qualquer tipo de modificar de acesso
+		Constructor<SubControle> constructorSubControleComParametros = 
+				subControleClasse1.getDeclaredConstructor(String.class);
+		System.out.println(constructorSubControleComParametros);
+		
+		
+		// Por padrao o newInstance do Constructor não permite recuperar construtores privados de uma classe,
+		//mas podemos mudar esse comportamento com o metodo setAccessible() passando 'true' como parametro.
+		constructorSubControle.setAccessible(true);
+		Object subControle = constructorSubControle.newInstance();
+		
+		System.out.println(subControle);
+		
+		
+	}
+
+}
