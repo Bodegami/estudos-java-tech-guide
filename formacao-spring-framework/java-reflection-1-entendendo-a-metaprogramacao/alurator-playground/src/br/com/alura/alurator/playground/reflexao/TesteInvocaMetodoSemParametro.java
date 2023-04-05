42e50da450bb390ed870c16f8a2cc6e16f69b0d7
Bodegami <rena.f.oliveira@gmail.com>
2023-04-04 22:29:21 -0300

    invocacao de metodos sem parametro

---
+++ formacao-spring-framework/java-reflection-1-entendendo-a-metaprogramacao/alurator-playground/src/br/com/alura/alurator/playground/reflexao/TesteInvocaMetodoSemParametro.java
@@ -0,0 +1,40 @@
+package br.com.alura.alurator.playground.reflexao;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class TesteInvocaMetodoSemParametro {
+
+    public static void main(String[] args) throws Exception {
+
+        Class<?> subControleClasse =
+                Class.forName("br.com.alura.alurator.playground.controle.SubControle");
+        
+      Constructor<?> construtorPadrao = subControleClasse.getDeclaredConstructor();
+      construtorPadrao.setAccessible(true);
+    
+      Object subControle = construtorPadrao.newInstance();
+
+        
+        for (Method m : subControleClasse.getMethods()) {
+        	System.out.println(m);
+        }
+        
+        System.out.println("\n--------------------><--------------------\n");
+        
+        for (Method m : subControleClasse.getDeclaredMethods()) {
+        	System.out.println(m);
+        }
+        
+        //Quando o metodo tem retorno, o invoke retorna Object
+        //Quando o metodo nao tem retorno, o invoke retorna null
+        
+        
+        Method method = subControleClasse.getDeclaredMethod("metodoSubControle1");
+        method.setAccessible(true);
+        Object object = method.invoke(subControle);
+        
+        System.out.println(object);
+
+    }
+}
