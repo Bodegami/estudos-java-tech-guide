5abd19189da7482f71d1122892118f54b6891868
Bodegami <rena.f.oliveira@gmail.com>
2023-04-05 23:14:24 -0300

    finalizando a invocacao de metodos com parametros

--- formacao-spring-framework/java-reflection-1-entendendo-a-metaprogramacao/alurator/src/br/com/alura/alurator/reflexao/ManipuladorObjeto.java
+++ formacao-spring-framework/java-reflection-1-entendendo-a-metaprogramacao/alurator/src/br/com/alura/alurator/reflexao/ManipuladorObjeto.java
@@ -13,29 +13,21 @@ public class ManipuladorObjeto {
 	}
 
 	public ManipuladorMetodo getMetodo(String nomeMetodo, Map<String, Object> params) {
-		// 1) Pegar todos os métodos da classe.
-		// 2) Filtrar todos os métodos de modo que:
-		// 2.1) Tenham o mesmo nome informado pelo usuário;
-		// 2.2) Tenham a mesma quantidade de parâmetros passados na URL;
-		// 2.3) E que cada um dos parâmetros tenham os mesmos nomes e tipos iguais
-		// aos passados na URL.
-		// 3) Lançar uma RuntimeException caso nenhum método seja encontrado.
-		
-		Stream<Method> metodos = Stream.of(instancia.getClass().getDeclaredMethods());
-		Method metodoSelecionado = metodos.filter(metodo -> 
-												metodo.getName().equals(nomeMetodo)
-												&& metodo.getParameterCount() == params.values().size())
-											.findFirst()
-											.orElseThrow(() -> new RuntimeException("metodo nao encontrado!"));
-		
-		try {
-			Method metodo = instancia.getClass().getDeclaredMethod(nomeMetodo);
-			return new ManipuladorMetodo(instancia, metodo);
-		} catch (NoSuchMethodException | SecurityException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-		
+
+	    Stream<Method> metodos = Stream.of(instancia.getClass().getDeclaredMethods());
+	    Method metodoSelecionado = metodos.filter(metodo -> 
+	                                            metodo.getName().equals(nomeMetodo)
+	                                        &&    metodo.getParameterCount() == params.values().size()
+	                                        && Stream.of(metodo.getParameters())
+	                                            .allMatch(arg -> 
+	                                                    params.keySet().contains(arg.getName())
+	                                                    && params.get(arg.getName()).getClass().equals(arg.getType())
+	                                            )
+	                                    )
+	                                        .findFirst()
+	                                        .orElseThrow(() -> new RuntimeException("Método não encontrado!"));
+
+	    return new ManipuladorMetodo(instancia, metodoSelecionado, params);
 	}
 
 }
