67dc3c155ca7afaacc77bd7ee9bda8a492a2005d
Bodegami <rena.f.oliveira@gmail.com>
2023-04-09 17:23:17 -0300

    utilizando o padrao DAO

--- formacao-spring-framework/formacao-java-com-banco-de-dados/byte-bank-jdbc/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
+++ formacao-spring-framework/formacao-java-com-banco-de-dados/byte-bank-jdbc/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -31,32 +31,8 @@ public class ContaService {
     }
 
     public void abrir(DadosAberturaConta dadosDaConta) {
-        var cliente = new Cliente(dadosDaConta.dadosCliente());
-        var conta = new Conta(dadosDaConta.numero(), cliente);
-        if (contas.contains(conta)) {
-            throw new RegraDeNegocioException("Já existe outra conta aberta com o mesmo número!");
-        }
-
-        String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email) " +
-                "VALUES (?, ?, ?, ?, ?)";
-
         Connection conn = connection.recuperarConexao();
-
-        try {
-
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setInt(1, conta.getNumero());
-            ps.setBigDecimal(2, BigDecimal.ZERO);
-            ps.setString(3, conta.getTitular().getNome());
-            ps.setString(4, conta.getTitular().getCpf());
-            ps.setString(5, conta.getTitular().getEmail());
-
-            ps.execute();
-        }
-        catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
+        new ContaDAO(conn).salvar(dadosDaConta);
     }
 
     public void realizarSaque(Integer numeroDaConta, BigDecimal valor) {
