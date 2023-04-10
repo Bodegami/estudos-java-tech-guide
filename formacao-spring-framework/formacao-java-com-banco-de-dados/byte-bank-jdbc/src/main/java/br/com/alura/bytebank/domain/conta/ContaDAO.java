431e4e5bf31e85334b374162a35a6a17ec2d0674
Bodegami <rena.f.oliveira@gmail.com>
2023-04-10 00:20:32 -0300

    implementando o rollback e caso de falha no update

--- formacao-spring-framework/formacao-java-com-banco-de-dados/byte-bank-jdbc/src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
+++ formacao-spring-framework/formacao-java-com-banco-de-dados/byte-bank-jdbc/src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
@@ -110,20 +110,27 @@ public class ContaDAO {
     }
 
     public void alterar(Integer numero, BigDecimal valor) {
+        PreparedStatement ps;
         String sql = "UPDATE conta SET saldo = ? WHERE numero = ?";
-        PreparedStatement ps = null;
 
         try {
+            conn.setAutoCommit(false);
+
             ps = conn.prepareStatement(sql);
 
             ps.setBigDecimal(1, valor);
             ps.setInt(2, numero);
 
             ps.execute();
+            conn.commit();
             ps.close();
             conn.close();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
             throw new RuntimeException(e);
         }
     }
