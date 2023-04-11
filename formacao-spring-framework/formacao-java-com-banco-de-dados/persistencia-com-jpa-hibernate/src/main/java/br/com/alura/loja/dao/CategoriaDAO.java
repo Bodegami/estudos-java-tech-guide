53753deba09778a9ddc6d2963598413c3ee0a63b
Renato <rena.f.oliveira@gmail.com>
2023-04-11 12:14:37 -0300

    estados no delete da entidade

--- formacao-spring-framework/formacao-java-com-banco-de-dados/persistencia-com-jpa-hibernate/src/main/java/br/com/alura/loja/dao/CategoriaDAO.java
+++ formacao-spring-framework/formacao-java-com-banco-de-dados/persistencia-com-jpa-hibernate/src/main/java/br/com/alura/loja/dao/CategoriaDAO.java
@@ -16,7 +16,15 @@ public class CategoriaDAO {
         this.em.persist(categoria);
     }
 
+    //Merge pega um objeto em estado 'Detached' e muda para o estado 'Managed'
     public void atualizar(Categoria categoria) {
         this.em.merge(categoria);
+    }
+
+    //Primeiro com o Merge, pegamos um objeto em estado 'Detached' e mudamos para o estado 'Managed'
+    //Depois com o objeto em estado 'Managed', chamamos o 'remove' que muda o estado para 'Removed'
+    public void remover(Categoria categoria) {
+        categoria = this.em.merge(categoria);
+        this.em.remove(categoria);
     }
 }
