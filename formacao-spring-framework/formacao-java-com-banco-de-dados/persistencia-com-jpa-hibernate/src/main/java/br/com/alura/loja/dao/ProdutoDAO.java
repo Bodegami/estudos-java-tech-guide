2c8420f845ab5523672df0e4ac73e9dc1335ef47
Renato <rena.f.oliveira@gmail.com>
2023-04-11 13:10:25 -0300

    consultando entidades

--- formacao-spring-framework/formacao-java-com-banco-de-dados/persistencia-com-jpa-hibernate/src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
+++ formacao-spring-framework/formacao-java-com-banco-de-dados/persistencia-com-jpa-hibernate/src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
@@ -1,8 +1,10 @@
 package br.com.alura.loja.dao;
 
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class ProdutoDAO {
 
@@ -15,5 +17,27 @@ public class ProdutoDAO {
     public void cadastrar(Produto produto) {
         this.em.persist(produto);
     }
+
+    //Merge pega um objeto em estado 'Detached' e muda para o estado 'Managed'
+    public void atualizar(Produto produto) {
+        this.em.merge(produto);
+    }
+
+    //Primeiro com o Merge, pegamos um objeto em estado 'Detached' e mudamos para o estado 'Managed'
+    //Depois com o objeto em estado 'Managed', chamamos o 'remove' que muda o estado para 'Removed'
+    public void remover(Produto produto) {
+        produto = this.em.merge(produto);
+        this.em.remove(produto);
+    }
+
+    public Produto buscarPorId(Long id) {
+        return this.em.find(Produto.class, id);
+    }
+
+    public List<Produto> buscarTodos() {
+        String jpql = "SELECT p FROM Produto p";
+        return this.em.createQuery(jpql, Produto.class).getResultList();
+    }
+
 
 }
--- formacao-spring-framework/formacao-java-com-banco-de-dados/persistencia-com-jpa-hibernate/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ formacao-spring-framework/formacao-java-com-banco-de-dados/persistencia-com-jpa-hibernate/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,29 +1,59 @@
 package br.com.alura.loja.testes;
 
+import br.com.alura.loja.dao.CategoriaDAO;
+import br.com.alura.loja.dao.ProdutoDAO;
 import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
-        Categoria celulares = new Categoria("CELULARES");
+        cadastrarProduto();
+
+        //buscaPeloId();
 
         EntityManager em = JPAUtil.getEntityManager();
-        em.getTransaction().begin();
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+        List<Produto> todos = produtoDAO.buscarTodos();
+        todos.forEach(produto -> System.out.println(produto.getNome()));
 
-        em.persist(celulares);
-        celulares.setNome("XPTO");
+    }
 
-        em.flush();
-        em.clear();
+    private static void buscaPeloId() {
+        Long id = 1L;
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
 
-        celulares = em.merge(celulares);
-        celulares.setNome("1234");
-        em.flush();
+        Produto p = produtoDAO.buscarPorId(id);
+        System.out.println(p.getPreco());
+    }
 
-        em.remove(celulares);
-        em.flush();
+
+    private static void cadastrarProduto() {
+        Categoria celulares = new Categoria("CELULARES");
+        Produto produto = new Produto("Xiaomi Redmi",
+                "Celular maneiro", new BigDecimal("800"), celulares);
+
+        EntityManager em = JPAUtil.getEntityManager();
+        CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+
+        try {
+            em.getTransaction().begin();
+
+            categoriaDAO.cadastrar(celulares);
+            produtoDAO.cadastrar(produto);
+
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        em.close();
     }
 }
