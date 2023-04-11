cd457b955fbdf0d0b1d817f6ed1f8634253fd318
Bodegami <rena.f.oliveira@gmail.com>
2023-04-11 00:26:28 -0300

    mais mapeamentos

--- formacao-spring-framework/formacao-java-com-banco-de-dados/persistencia-com-jpa-hibernate/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ formacao-spring-framework/formacao-java-com-banco-de-dados/persistencia-com-jpa-hibernate/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,22 +1,19 @@
 package br.com.alura.loja.testes;
 
 import br.com.alura.loja.dao.ProdutoDAO;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import java.math.BigDecimal;
 
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
 
-        Produto produto = new Produto();
-        produto.setNome("Xiaomi Redmi");
-        produto.setDescricao("Celular maneiro");
-        produto.setPreco(new BigDecimal("800"));
+        Produto produto = new Produto("Xiaomi Redmi",
+                "Celular maneiro", new BigDecimal("800"), Categoria.CELULARES);
 
         EntityManager em = JPAUtil.getEntityManager();
         ProdutoDAO dao = new ProdutoDAO(em);
