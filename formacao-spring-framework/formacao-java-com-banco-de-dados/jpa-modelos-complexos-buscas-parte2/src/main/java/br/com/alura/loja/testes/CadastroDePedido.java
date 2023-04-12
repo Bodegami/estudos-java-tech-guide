0d1711a2558eb753fc26d1559612a6bb2bfa9d85
Bodegami <rena.f.oliveira@gmail.com>
2023-04-12 00:21:12 -0300

    consultas com o select new

--- formacao-spring-framework/formacao-java-com-banco-de-dados/jpa-modelos-complexos-buscas-parte2/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
+++ formacao-spring-framework/formacao-java-com-banco-de-dados/jpa-modelos-complexos-buscas-parte2/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
@@ -6,6 +6,7 @@ import br.com.alura.loja.dao.PedidoDao;
 import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.*;
 import br.com.alura.loja.util.JPAUtil;
+import br.com.alura.loja.vo.RelatorioDeVendasVO;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
@@ -20,16 +21,23 @@ public class CadastroDePedido {
         ProdutoDao produtoDao = new ProdutoDao(em);
         ClienteDao clienteDao = new ClienteDao(em);
 
-        Produto produto = produtoDao.buscarPorId(1L);
+        Produto produto1 = produtoDao.buscarPorId(1L);
+        Produto produto2 = produtoDao.buscarPorId(2L);
+        Produto produto3 = produtoDao.buscarPorId(3L);
         Cliente cliente = clienteDao.buscarPorId(1L);
 
         em.getTransaction().begin();
 
-        Pedido pedido = new Pedido(cliente);
-        pedido.adicionarItem(new ItemPedido(10, pedido, produto));
+        Pedido pedido1 = new Pedido(cliente);
+        pedido1.adicionarItem(new ItemPedido(10, pedido1, produto1));
+        pedido1.adicionarItem(new ItemPedido(5, pedido1, produto2));
+
+        Pedido pedido2 = new Pedido(cliente);
+        pedido2.adicionarItem(new ItemPedido(10, pedido2, produto3));
 
         PedidoDao pedidoDao = new PedidoDao(em);
-        pedidoDao.cadastrar(pedido);
+        pedidoDao.cadastrar(pedido1);
+        pedidoDao.cadastrar(pedido2);
 
         em.getTransaction().commit();
 
@@ -37,17 +45,26 @@ public class CadastroDePedido {
         BigDecimal valorTotal = pedidoDao.valorTotalVendido();
         System.out.println("VALOR TOTAL: " + valorTotal);
 
-        List<Object[]> relatorio = pedidoDao.relatorioDeVendas();
-        for (Object[] obj: relatorio) {
-            System.out.println(obj[0]);
-            System.out.println(obj[1]);
-            System.out.println(obj[2]);
-        }
+//        List<Object[]> relatorio = pedidoDao.relatorioDeVendas();
+//        for (Object[] obj: relatorio) {
+//            System.out.println(obj[0]);
+//            System.out.println(obj[1]);
+//            System.out.println(obj[2]);
+//            System.out.println();
+//        }
+
+        List<RelatorioDeVendasVO> relatorios = pedidoDao.relatorioDeVendas();
+        relatorios.forEach(System.out::println);
     }
 
     private static void popularBancoDeDados() {
         Categoria celulares = new Categoria("CELULARES");
+        Categoria videoGames = new Categoria("PS5");
+        Categoria headsets = new Categoria("HEADSETS");
+
         Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares );
+        Produto videoGame = new Produto("Playstation 5", "Muito legal", new BigDecimal("4000"), videoGames );
+        Produto headset = new Produto("Logitech G435", "Muito legal", new BigDecimal("280"), headsets );
 
         Cliente cliente = new Cliente("Renato", "12345");
 
@@ -59,7 +76,15 @@ public class CadastroDePedido {
         em.getTransaction().begin();
 
         categoriaDao.cadastrar(celulares);
+        categoriaDao.cadastrar(videoGames);
+        categoriaDao.cadastrar(headsets);
+
+
         produtoDao.cadastrar(celular);
+        produtoDao.cadastrar(videoGame);
+        produtoDao.cadastrar(headset);
+
+
         clienteDao.cadastrar(cliente);
 
         em.getTransaction().commit();
