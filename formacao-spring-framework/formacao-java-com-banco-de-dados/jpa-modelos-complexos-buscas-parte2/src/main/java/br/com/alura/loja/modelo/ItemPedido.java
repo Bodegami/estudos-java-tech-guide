02d6de24bdb7bac091a6fb2c92d4d9f8f97afb94
Bodegami <rena.f.oliveira@gmail.com>
2023-04-11 21:17:00 -0300

    relacionamentos many-to-one

---
+++ formacao-spring-framework/formacao-java-com-banco-de-dados/jpa-modelos-complexos-buscas-parte2/src/main/java/br/com/alura/loja/modelo/ItemPedido.java
@@ -0,0 +1,50 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "itens_pedido")
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal precoUnitario;
+    private int quantidade;
+
+    @ManyToOne
+    private Pedido pedido;
+
+    @ManyToOne
+    private Produto produto;
+
+    public ItemPedido() {
+    }
+
+    public ItemPedido(int quantidade, Pedido pedido, Produto produto) {
+        this.quantidade = quantidade;
+        this.pedido = pedido;
+        this.produto = produto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+}
