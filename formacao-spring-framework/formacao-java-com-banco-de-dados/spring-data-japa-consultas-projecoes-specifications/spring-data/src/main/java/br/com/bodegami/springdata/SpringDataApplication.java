3e804f37ada378d7ab8e53802b91ae21a61fc862
Renato <rena.f.oliveira@gmail.com>
2023-04-18 23:52:05 -0300

    inserindo valores

--- formacao-spring-framework/formacao-java-com-banco-de-dados/spring-data-japa-consultas-projecoes-specifications/spring-data/src/main/java/br/com/bodegami/springdata/SpringDataApplication.java
+++ formacao-spring-framework/formacao-java-com-banco-de-dados/spring-data-japa-consultas-projecoes-specifications/spring-data/src/main/java/br/com/bodegami/springdata/SpringDataApplication.java
@@ -1,13 +1,28 @@
 package br.com.bodegami.springdata;
 
+import br.com.bodegami.springdata.orm.Cargo;
+import br.com.bodegami.springdata.repository.CargoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
+
+	private final CargoRepository repository;
+
+	public SpringDataApplication(CargoRepository repository) {
+		this.repository = repository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
+		repository.save(cargo);
+	}
 }
