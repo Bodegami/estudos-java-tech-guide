77b521ec26338f2f0e95975caad80d506e335381
Renato <rena.f.oliveira@gmail.com>
2023-04-20 23:18:27 -0300

    Usando JPQL

--- formacao-spring-framework/formacao-java-com-banco-de-dados/spring-data-japa-consultas-projecoes-specifications/spring-data/src/main/java/br/com/bodegami/springdata/repository/FuncionarioRepository.java
+++ formacao-spring-framework/formacao-java-com-banco-de-dados/spring-data-japa-consultas-projecoes-specifications/spring-data/src/main/java/br/com/bodegami/springdata/repository/FuncionarioRepository.java
@@ -1,14 +1,22 @@
 package br.com.bodegami.springdata.repository;
 
 import br.com.bodegami.springdata.orm.Funcionario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
 
     List<Funcionario> findByNome(String nome);
+
+    //Metodo com Derived QUery
+    //List<Funcionario> findByNomeAndSalarioGreaterThanAndDataContratacao(String nome, Double salario, LocalDate data);
+
+    @Query("SELECT f FROM Funcionario f WHERE f.nome = :nome AND f.salario >= :salario AND f.dataContratacao = :data")
+    List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, Double Salario, LocalDate data);
 
 }
