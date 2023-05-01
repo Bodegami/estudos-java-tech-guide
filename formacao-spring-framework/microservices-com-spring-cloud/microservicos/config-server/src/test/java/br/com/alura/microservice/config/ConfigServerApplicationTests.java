598d6f23b13e27f37ccc53afbccdc28b1d63e9da
Renato <rena.f.oliveira@gmail.com>
2023-04-30 22:26:15 -0300

    criando o servidor de configuracao

---
+++ formacao-spring-framework/microservices-com-spring-cloud/microservicos/config-server/src/test/java/br/com/alura/microservice/config/ConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.alura.microservice.config;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
