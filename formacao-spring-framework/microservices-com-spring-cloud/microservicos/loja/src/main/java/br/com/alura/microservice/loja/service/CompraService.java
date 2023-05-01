4f1bc0dec331fce05e52eda3db18355d25230113
Renato <rena.f.oliveira@gmail.com>
2023-05-01 19:33:02 -0300

    sobre Logs e Distributed Tracing

--- formacao-spring-framework/microservices-com-spring-cloud/microservicos/loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ formacao-spring-framework/microservices-com-spring-cloud/microservicos/loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.loja.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,23 +14,25 @@ import br.com.alura.microservice.loja.model.Compra;
 @Service
 public class CompraService {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
+	
 	@Autowired
 	private FornecedorClient client;
 
 	public Compra realizaCompra(CompraDTO dto) {
 		
+		final String estado = dto.getEndereco().getEstado();
+		
+		LOG.info("bucando informações do fornecedor de {}", estado);
 		InfoFornecedorDTO info = client.getInfoPorEstado(dto.getEndereco().getEstado());
 		
+		LOG.info("realizando um pedido");
 		InfoPedidoDTO pedido = client.realizaPedido(dto.getItens());
-		
-		System.out.println(info.getEndereco());
 		
 		Compra compra = new Compra();
 		compra.setPedidoId(pedido.getId());
 		compra.setTempoDePreparo(pedido.getTempoDePreparo());
 		compra.setEnderecoDestino(dto.getEndereco().toString());
-		
-		System.out.println(compra);
 		
 		return compra;
 	}
