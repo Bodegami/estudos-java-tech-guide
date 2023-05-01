ce8ebc8f1ea30d0ee0372b2e2ea6a235d811699a
Renato <rena.f.oliveira@gmail.com>
2023-05-01 19:19:56 -0300

    implementando pedido e produtos no fornecedor

--- formacao-spring-framework/microservices-com-spring-cloud/microservicos/loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ formacao-spring-framework/microservices-com-spring-cloud/microservicos/loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.dto.CompraDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
+import br.com.alura.microservice.loja.dto.InfoPedidoDTO;
+import br.com.alura.microservice.loja.model.Compra;
 
 @Service
 public class CompraService {
@@ -13,10 +15,21 @@ public class CompraService {
 	@Autowired
 	private FornecedorClient client;
 
-	public void realizaCompra(CompraDTO compra) {
+	public Compra realizaCompra(CompraDTO dto) {
 		
-		InfoFornecedorDTO info = client.getInfoPorEstado(compra.getEndereco().getEstado());
+		InfoFornecedorDTO info = client.getInfoPorEstado(dto.getEndereco().getEstado());
+		
+		InfoPedidoDTO pedido = client.realizaPedido(dto.getItens());
 		
 		System.out.println(info.getEndereco());
+		
+		Compra compra = new Compra();
+		compra.setPedidoId(pedido.getId());
+		compra.setTempoDePreparo(pedido.getTempoDePreparo());
+		compra.setEnderecoDestino(dto.getEndereco().toString());
+		
+		System.out.println(compra);
+		
+		return compra;
 	}
 }
