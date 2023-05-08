d60bb695efca4304d509d1662af31bce05385d91
Renato <rena.f.oliveira@gmail.com>
2023-05-08 20:44:41 -0300

    inicio curso Threads-java-parte-1 && visualizando a primeira Thread

---
+++ formacao-threads/threads/src/br/com/alura/threads/Principal.java
@@ -0,0 +1,13 @@
+package br.com.alura.threads;
+
+public class Principal {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.out.println("essa é a Thread 'main'");
+		
+		Thread.sleep(50000);
+		
+	}
+
+}
