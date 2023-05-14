89ff235fb7bfeac2d7466cb1e55c1702dbc7b501
Renato <rena.f.oliveira@gmail.com>
2023-05-13 22:42:48 -0300

    verificando quantidade de processadores e threads em execucao

--- formacao-threads/threads-em-java-parte-2/servidor-tarefas/src/br/com/alura/servidor/ServidorTarefas.java
+++ formacao-threads/threads-em-java-parte-2/servidor-tarefas/src/br/com/alura/servidor/ServidorTarefas.java
@@ -2,8 +2,8 @@ package br.com.alura.servidor;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class ServidorTarefas {
@@ -13,8 +13,8 @@ public class ServidorTarefas {
 		System.out.println("---- Iniciando o servidor -----");
 		ServerSocket servidor = new ServerSocket(12345);
 		//ExecutorService threadPool = Executors.newFixedThreadPool(2);
-		ScheduledExecutorService pool = Executors.newScheduledThreadPool(4);
-		//ExecutorService threadPool = Executors.newCachedThreadPool();
+		//ScheduledExecutorService pool = Executors.newScheduledThreadPool(4);
+		ExecutorService threadPool = Executors.newCachedThreadPool();
 		
 		while (true) {
 			Socket socket = servidor.accept();		
@@ -22,8 +22,8 @@ public class ServidorTarefas {
 			
 			DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
 
-			pool.scheduleAtFixedRate(distribuirTarefas, 0, 60, TimeUnit.MINUTES); //executamos uma tarefa a cada 60 minutos
-			//threadPool.execute(distribuirTarefas);
+			//pool.scheduleAtFixedRate(distribuirTarefas, 0, 60, TimeUnit.MINUTES); //executamos uma tarefa a cada 60 minutos
+			threadPool.execute(distribuirTarefas);
 		}
 		
 	}
---
+++ formacao-threads/threads-em-java-parte-2/servidor-tarefas/src/br/com/alura/servidor/VerificaThreadEmExecucao.java
@@ -0,0 +1,21 @@
+package br.com.alura.servidor;
+
+import java.util.Set;
+
+public class VerificaThreadEmExecucao {
+
+	public static void main(String[] args) {
+
+
+		Set<Thread> todasAsThreads = Thread.getAllStackTraces().keySet();
+
+		for (Thread thread : todasAsThreads) {
+		    System.out.println(thread.getName());
+		}
+
+	    Runtime runtime = Runtime.getRuntime();
+	    int qtdProcessadores = runtime.availableProcessors();
+	    System.out.println("Qtd de processadores: " + qtdProcessadores);
+	}
+
+}
