20afd591b274e01d31defaf5d3760ad4f8c8ec9b
Renato <rena.f.oliveira@gmail.com>
2023-05-13 21:06:20 -0300

    inputStream e outputStream

--- formacao-threads/threads-em-java-parte-2/servidor-tarefas/src/br/com/alura/cliente/ClienteTarefas.java
+++ formacao-threads/threads-em-java-parte-2/servidor-tarefas/src/br/com/alura/cliente/ClienteTarefas.java
@@ -1,5 +1,6 @@
 package br.com.alura.cliente;
 
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -11,10 +12,14 @@ public class ClienteTarefas {
 		
 		System.out.println("conexao estabelicida");
 		
-		Scanner teclado = new Scanner(System.in);
+		PrintStream saida = new PrintStream(socket.getOutputStream());
+		saida.println("c1");
 		
+		Scanner teclado = new Scanner(System.in);
 		teclado.nextLine();
 		
+		saida.close();
+		teclado.close();
 		socket.close();
 	}
 
