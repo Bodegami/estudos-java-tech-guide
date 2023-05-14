5f117ff90e1cb9aa692c4eb2b93eccc3f22bf76c
Renato <rena.f.oliveira@gmail.com>
2023-05-13 21:21:11 -0300

    Thread Pool com schedule

--- formacao-threads/threads-em-java-parte-2/servidor-tarefas/src/br/com/alura/servidor/ServidorTarefas.java
+++ formacao-threads/threads-em-java-parte-2/servidor-tarefas/src/br/com/alura/servidor/ServidorTarefas.java
@@ -2,8 +2,9 @@ package br.com.alura.servidor;
 
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ServidorTarefas {
 	public static void main(String[] args) throws Exception {
@@ -12,14 +13,17 @@ public class ServidorTarefas {
 		System.out.println("---- Iniciando o servidor -----");
 		ServerSocket servidor = new ServerSocket(12345);
 		//ExecutorService threadPool = Executors.newFixedThreadPool(2);
-		ExecutorService threadPool = Executors.newCachedThreadPool();
+		ScheduledExecutorService pool = Executors.newScheduledThreadPool(4);
+		//ExecutorService threadPool = Executors.newCachedThreadPool();
 		
 		while (true) {
 			Socket socket = servidor.accept();		
 			System.out.println("Aceitando novo cliente na porta: " + socket.getPort());
 			
 			DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
-			threadPool.execute(distribuirTarefas);
+
+			pool.scheduleAtFixedRate(distribuirTarefas, 0, 60, TimeUnit.MINUTES); //executamos uma tarefa a cada 60 minutos
+			//threadPool.execute(distribuirTarefas);
 		}
 		
 	}
