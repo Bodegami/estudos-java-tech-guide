237875139b5b3597402f84b2986e52860c8468b4
Renato <rena.f.oliveira@gmail.com>
2023-05-13 21:14:50 -0300

    Thread Pool

--- formacao-threads/threads-em-java-parte-2/servidor-tarefas/src/br/com/alura/servidor/ServidorTarefas.java
+++ formacao-threads/threads-em-java-parte-2/servidor-tarefas/src/br/com/alura/servidor/ServidorTarefas.java
@@ -2,6 +2,8 @@ package br.com.alura.servidor;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServidorTarefas {
 	public static void main(String[] args) throws Exception {
@@ -9,16 +11,15 @@ public class ServidorTarefas {
 		
 		System.out.println("---- Iniciando o servidor -----");
 		ServerSocket servidor = new ServerSocket(12345);
+		//ExecutorService threadPool = Executors.newFixedThreadPool(2);
+		ExecutorService threadPool = Executors.newCachedThreadPool();
 		
 		while (true) {
 			Socket socket = servidor.accept();		
 			System.out.println("Aceitando novo cliente na porta: " + socket.getPort());
 			
 			DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
-			Thread threadCliente = new Thread(distribuirTarefas);
-			threadCliente.start();
-			
-			
+			threadPool.execute(distribuirTarefas);
 		}
 		
 	}
