6a9bf967506852036a273c6926c96e279156c487
Renato <rena.f.oliveira@gmail.com>
2023-05-20 20:40:04 -0300

    Tratando erro 404

--- formacao-spring-framework/formacao-spring-boot/spring-boot-3-parte-2/api/src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ formacao-spring-framework/formacao-spring-boot/spring-boot-3-parte-2/api/src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -1,51 +1,49 @@
-package med.voll.api.medico;
+package med.voll.api.domain.paciente;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import med.voll.api.endereco.Endereco;
-
-import java.util.stream.Stream;
+import med.voll.api.domain.endereco.Endereco;
 
 @Getter
+@EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
-@Entity(name = "Medico")
-@Table(name = "medicos")
-public class Medico {
+@Entity(name = "Paciente")
+@Table(name = "pacientes")
+public class Paciente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String email;
+    private String cpf;
     private String telefone;
-    private String crm;
-
-    @Enumerated(EnumType.STRING)
-    private Especialidade especialidade;
 
     @Embedded
     private Endereco endereco;
 
     private boolean ativo;
 
-    public Medico(DadosCadastroMedico dados) {
+    public Paciente(DadosCadastroPaciente dados) {
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
-        this.crm = dados.crm();
-        this.especialidade = dados.especialidade();
+        this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
         this.ativo = true;
     }
 
-    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+    public void atualizar(DadosAtualizacaoPaciente dados) {
         if (dados.nome() != null) {
             this.nome = dados.nome();
+        }
+        if (dados.email() != null) {
+            this.email = dados.email();
         }
         if (dados.telefone() != null) {
             this.telefone = dados.telefone();
