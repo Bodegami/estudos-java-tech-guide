29bddd4f5db840a16c4328e7316ea9167d58ce50
Renato <rena.f.oliveira@gmail.com>
2023-05-20 18:22:47 -0300

    inicio curso spring-boot-3-parte-2

---
+++ formacao-spring-framework/formacao-spring-boot/spring-boot-3-parte-2/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,58 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Medico> cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+        Medico medico = repository.save(new Medico(dados));
+        return ResponseEntity.ok(medico);
+    }
+
+    @GetMapping
+    //O pageable é opcional na requisicao, se for enviado na request o Spring utilizara, caso contrario é usado o default
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    //Como o metodo é anotado com a @Transactional, a Jpa abre uma transacao ee a partir disso carregamos uma
+    //entidade do banco. Qualquer alteração feita nessa entidade, será comitada no banco quando o metodo encerrar
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+        Medico medico = repository.getReferenceById(dados.id());
+        medico.atualizar(dados);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
+    }
+
+    //Exclusão do Banco de dados
+//    @DeleteMapping(value = "/{id}")
+//    @Transactional
+//    public void excluir(@PathVariable Long id) {
+//        repository.deleteById(id);
+//    }
+
+
+}
---
+++ formacao-spring-framework/formacao-spring-boot/spring-boot-3-parte-2/api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,56 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.paciente.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    @Transactional
+    public Page<DadosListagemPaciente> findAll(@PageableDefault(
+            size = 2, page = 0, sort = {"cpf"}, direction = Sort.Direction.DESC) Pageable paginacao) {
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+        Paciente paciente = repository.getReferenceById(dados.id());
+        paciente.atualizar(dados);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        Paciente paciente = repository.getReferenceById(id);
+        paciente.excluir();
+    }
+
+
+    //Exclusão definitiva do banco de dados
+//    @DeleteMapping(value = "/{id}")
+//    @Transactional
+//    public void excluir(@PathVariable Long id) {
+//        repository.deleteById(id);
+//    }
+}
