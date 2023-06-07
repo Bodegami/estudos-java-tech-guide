c762f36fd15eb0ad4efbb189874141d0d464360b
Renato <rena.f.oliveira@gmail.com>
2023-06-06 21:20:21 -0300

    Classes de validacao

---
+++ formacao-spring-framework/formacao-spring-boot/spring-boot-3-parte-3/api/src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,24 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class ValidadorHorarioAntecedencia {
+
+    public void validar(DadosAgendamentoConsulta dados) {
+
+        LocalDateTime dataConsulta = dados.data();
+        LocalDateTime agora = LocalDateTime.now();
+        long diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
+
+        if (diferencaEmMinutos < 30) {
+            throw new ValidacaoException("Consulta deve ser agendada com antecedência mínima de 30 minutos!");
+        }
+
+
+
+    }
+}
---
+++ formacao-spring-framework/formacao-spring-boot/spring-boot-3-parte-3/api/src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -0,0 +1,26 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+public class ValidadorHorarioFuncionamentoClinica {
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        LocalDateTime dataConsulta = dados.data();
+
+        boolean ehDomingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        boolean antesDaAberturaDaClinica = dataConsulta.getHour() < 7;
+        boolean depoisDoEncerramentoDaClinica = dataConsulta.getHour() > 18;
+
+        if (ehDomingo || antesDaAberturaDaClinica || depoisDoEncerramentoDaClinica) {
+            throw new ValidacaoException("Consulta fora do horário de funcionamento da clínica!");
+        }
+
+
+
+    }
+
+}
