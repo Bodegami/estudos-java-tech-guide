30e9ce81e22a52c32fe6a46f1b633ecb5e7768e5
Renato <rena.f.oliveira@gmail.com>
2023-06-14 22:42:35 -0300

    Criando a VPC

--- formacao-java-microsservicos/microservicos-iac-com-cdk-e-deploy-na-aws/alura-aws-infra/src/main/java/com/myorg/AluraAwsInfraApp.java
+++ formacao-java-microsservicos/microservicos-iac-com-cdk-e-deploy-na-aws/alura-aws-infra/src/main/java/com/myorg/AluraAwsInfraApp.java
@@ -3,6 +3,7 @@ package com.myorg;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
 
 import java.util.Arrays;
 
@@ -10,13 +11,16 @@ public class AluraAwsInfraApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new AluraAwsInfraStack(app, "AluraAwsInfraStack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+        new AluraVpcStack(app, "Vpc");
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
+        app.synth();
+
+        // If you don't specify 'env', this stack will be environment-agnostic.
+        // Account/Region-dependent features and context lookups will not work,
+        // but a single synthesized template can be deployed anywhere.
+
+        // Uncomment the next block to specialize this stack for the AWS Account
+        // and Region that are implied by the current CLI configuration.
                 /*
                 .env(Environment.builder()
                         .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
@@ -24,8 +28,8 @@ public class AluraAwsInfraApp {
                         .build())
                 */
 
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
+        // Uncomment the next block if you know exactly what Account and Region you
+        // want to deploy the stack to.
                 /*
                 .env(Environment.builder()
                         .account("123456789012")
@@ -33,10 +37,7 @@ public class AluraAwsInfraApp {
                         .build())
                 */
 
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                .build());
-
-        app.synth();
+        // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     }
 }
 
---
+++ formacao-java-microsservicos/microservicos-iac-com-cdk-e-deploy-na-aws/alura-aws-infra/src/main/java/com/myorg/AluraVpcStack.java
@@ -0,0 +1,26 @@
+package com.myorg;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.constructs.Construct;
+
+
+public class AluraVpcStack extends Stack {
+    public AluraVpcStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public AluraVpcStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        Vpc vpc = Vpc.Builder.create(this, "AluraVpc")
+                .maxAzs(3)  // Default is all AZs in region
+                .build();
+
+        // example resource
+        // final Queue queue = Queue.Builder.create(this, "AluraAwsInfraQueue")
+        //         .visibilityTimeout(Duration.seconds(300))
+        //         .build();
+    }
+}
