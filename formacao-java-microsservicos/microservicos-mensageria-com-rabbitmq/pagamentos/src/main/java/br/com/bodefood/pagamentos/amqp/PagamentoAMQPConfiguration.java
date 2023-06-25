e329f8f3a5acf807e1c27a39e6fef7ef3f689e58
Renato <rena.f.oliveira@gmail.com>
2023-06-24 22:17:05 -0300

    Transferindo representações

--- formacao-java-microsservicos/microservicos-mensageria-com-rabbitmq/pagamentos/src/main/java/br/com/bodefood/pagamentos/amqp/PagamentoAMQPConfiguration.java
+++ formacao-java-microsservicos/microservicos-mensageria-com-rabbitmq/pagamentos/src/main/java/br/com/bodefood/pagamentos/amqp/PagamentoAMQPConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -33,7 +35,22 @@ public class PagamentoAMQPConfiguration {
         return event -> rabbitAdmin.initialize();
     }
 
+    // Cria um conversor para json que substituira o default (SimpleMessageConverter),
+    // que é utilizado pelo metodo converterAndSend()
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 
+    //Define que ao cria um RabbitTemplate, o mesmo ira utilizar o nosso Jackson2JsonMessageConverter
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter messageConverter) {
 
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+
+    }
 
 }
