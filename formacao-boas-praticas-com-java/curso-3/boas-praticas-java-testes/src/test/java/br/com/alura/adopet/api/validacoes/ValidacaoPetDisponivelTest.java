2c6754d15aaa523fcc8ce0db4ca1efc1a68ef062
Renato <rena.f.oliveira@gmail.com>
2024-01-30 20:31:08 -0300

    manipulando mocks

--- formacao-boas-praticas-com-java/curso-3/boas-praticas-java-testes/src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
+++ formacao-boas-praticas-com-java/curso-3/boas-praticas-java-testes/src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
@@ -1,12 +1,16 @@
 package br.com.alura.adopet.api.validacoes;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.repository.PetRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,21 +21,35 @@ class ValidacaoPetDisponivelTest {
     @Mock
     private PetRepository petRepository;
 
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private SolicitacaoAdocaoDto dto;
+
     @InjectMocks
     private ValidacaoPetDisponivel validacao;
 
-    @DisplayName("")
+    @DisplayName("Deve permitir solicitacao de adocao pet")
     @Test
     void deveriaPermitirSolicitacaoDeAdocaoPet() {
         //ARRANGE
-        SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(
-                7l,
-                2l,
-                "Motivo qualquer"
-        );
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(false);
 
         //ASSERT + ACT
         assertDoesNotThrow(() -> validacao.validar(dto));
+    }
+
+    @DisplayName("Nao deve permitir solicitacao de adocao pet")
+    @Test
+    void naoDeveriaPermitirSolicitacaoDeAdocaoPet() {
+        //ARRANGE
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(true);
+
+        //ASSERT + ACT
+        assertThrows(ValidacaoException.class, () -> validacao.validar(dto));
     }
 
 }
