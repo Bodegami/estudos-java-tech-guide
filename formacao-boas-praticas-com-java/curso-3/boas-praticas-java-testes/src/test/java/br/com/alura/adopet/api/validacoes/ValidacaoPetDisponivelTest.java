15404619325a3678cc9d99b1782694a05917a367
Renato <rena.f.oliveira@gmail.com>
2024-01-30 20:22:21 -0300

    utilizando mocks

--- formacao-boas-praticas-com-java/curso-3/boas-praticas-java-testes/src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
+++ formacao-boas-praticas-com-java/curso-3/boas-praticas-java-testes/src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
@@ -1,12 +1,24 @@
 package br.com.alura.adopet.api.validacoes;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.repository.PetRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class ValidacaoPetDisponivelTest {
+
+    @Mock
+    private PetRepository petRepository;
+
+    @InjectMocks
+    private ValidacaoPetDisponivel validacao;
 
     @DisplayName("")
     @Test
@@ -17,9 +29,6 @@ class ValidacaoPetDisponivelTest {
                 2l,
                 "Motivo qualquer"
         );
-
-        ValidacaoPetDisponivel validacao = new ValidacaoPetDisponivel();
-
 
         //ASSERT + ACT
         assertDoesNotThrow(() -> validacao.validar(dto));
