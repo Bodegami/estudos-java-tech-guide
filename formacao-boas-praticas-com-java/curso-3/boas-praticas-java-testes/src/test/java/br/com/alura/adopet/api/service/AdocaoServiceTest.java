62cd1ae0523ecd9b3506fc1f383e04082c419aed
Renato <rena.f.oliveira@gmail.com>
2024-01-30 20:50:19 -0300

    validando objetos com ArgumentCaptor

--- formacao-boas-praticas-com-java/curso-3/boas-praticas-java-testes/src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
+++ formacao-boas-praticas-com-java/curso-3/boas-praticas-java-testes/src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -2,6 +2,7 @@ package br.com.alura.adopet.api.service;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
@@ -10,10 +11,7 @@ import br.com.alura.adopet.api.repository.TutorRepository;
 import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
@@ -52,16 +50,29 @@ class AdocaoServiceTest {
 
     private SolicitacaoAdocaoDto dto;
 
+    @Captor
+    private ArgumentCaptor<Adocao> adocaoCaptor;
+
 
     @Test
     void deveriaSalvarAdocaoAoSolicitar() {
         //ARRANGE
+        this.dto = new SolicitacaoAdocaoDto(10L, 20L, "motivo qualquer!");
+
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+        BDDMockito.given(pet.getAbrigo()).willReturn(abrigo);
+
 
         //ACT
         service.solicitar(dto);
 
         //ASSERT
-        BDDMockito.then(petRepository).should().save(Mockito.any());
+        BDDMockito.then(repository).should().save(adocaoCaptor.capture());
+        Adocao adocaoSalva = adocaoCaptor.getValue();
+        assertEquals(pet, adocaoSalva.getPet());
+        assertEquals(tutor, adocaoSalva.getTutor());
+        assertEquals(dto.motivo(), adocaoSalva.getMotivo());
     }
 
 
