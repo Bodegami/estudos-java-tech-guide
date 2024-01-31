9321e8db511ed35e1c1353768feb103c181dc84e
Renato <rena.f.oliveira@gmail.com>
2024-01-30 21:22:25 -0300

    simulando requisicoes

--- formacao-boas-praticas-com-java/curso-3/boas-praticas-java-testes/src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ formacao-boas-praticas-com-java/curso-3/boas-praticas-java-testes/src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -1,25 +1,39 @@
 package br.com.alura.adopet.api.controller;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class AdocaoControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
     @Test
-    void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() {
+    void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() throws Exception {
         //ARRANGE
         String json = "{}";
 
         //ACT
-        
-
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
 
         //ASSERT
-        assertEquals(400, ?);
+        assertEquals(400, response.getStatus());
 
     }
 
